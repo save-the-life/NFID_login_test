bda06ff67d0850915ae03f8eb37b33743c872c3a
JungHo-K1m <qopqop55@naver.com>
2024-08-07 14:35:24 +0900

    add explanations

--- src/NFID_login_backend/src/lib.rs
+++ src/NFID_login_backend/src/lib.rs
@@ -5,8 +5,8 @@ use std::collections::HashMap;
 
 #[derive(CandidType, Serialize, Deserialize, Clone)]
 struct User {
-    user_principal: Principal,
-    wallet_address: String,
+    user_principal: Principal,  // user's principal
+    wallet_address: String,     // user's wallet address
 }
 
 type Users = HashMap<Principal, User>;
@@ -35,6 +35,7 @@ fn pre_upgrade() {
         let users = users.borrow();
         PRINCIPALS.with(|principals| {
             let principals = principals.borrow();
+            // store USERS and PRINCIPALS at stable memory
             ic_cdk::storage::stable_save((users.clone(), principals.clone())).expect("Failed to save stable storage.");
         });
     });
@@ -43,7 +44,10 @@ fn pre_upgrade() {
 // post_upgrade to use stable memory
 #[post_upgrade]
 fn post_upgrade() {
+    // restore the data from stable memory
     let result: Result<(Users, Vec<Principal>), _> = ic_cdk::storage::stable_restore();
+
+    // store the restored data at USERS and PRINCIPALS
     match result {
         Ok((saved_users, saved_principals)) => {
             USERS.with(|users| {
@@ -73,8 +77,10 @@ fn add_user(principal: Principal) -> Result<(), String> {
                 user_principal: principal.clone(),
                 wallet_address,
             };
+            // add new user's data to USERS HashMap
             users.insert(principal.clone(), user);
             PRINCIPALS.with(|principals| {
+                // add new princiapl to PRINCIPALS vector
                 principals.borrow_mut().push(principal);
             });
             Ok(())
@@ -88,6 +94,7 @@ fn add_user(principal: Principal) -> Result<(), String> {
 fn get_user(principal: Principal) -> Option<User> {
     USERS.with(|users| {
         let users = users.borrow();
+        // return the user corresponding to the given principal
         users.get(&principal).cloned()
     })
 }
@@ -96,6 +103,7 @@ fn get_user(principal: Principal) -> Option<User> {
 #[query]
 fn get_all_principals() -> Vec<Principal> {
     PRINCIPALS.with(|principals| {
+        // return the vector of all principals
         principals.borrow().clone()
     })
 }
--- src/NFID_login_frontend/src/features/Auth/model/useAuth.js
+++ src/NFID_login_frontend/src/features/Auth/model/useAuth.js
@@ -15,17 +15,23 @@ export const useAuth = () => {
       const client = await AuthClient.create();
       setAuthClient(client);
 
+
+      // get principal and sessionExpiration from local storage
       const storedPrincipal = localStorage.getItem('principal');
       const storedExpiresAt = localStorage.getItem('expiresAt');
+
+      // if both of them are not empty, set the values on the principal and sessionExpiry
       if (storedPrincipal && storedExpiresAt) {
         setPrincipal(storedPrincipal);
         setSessionExpiry(parseInt(storedExpiresAt, 10));
       }
     };
+
+    // 
     initAuthClient();
   }, []);
 
-  //check session per 10 second
+  //check session every 10 seconds
   useEffect(() => {
     if (sessionExpiry) {
       const timer = setInterval(() => {
@@ -40,19 +46,28 @@ export const useAuth = () => {
 
   // Internet Identity login
   const loginII = async (event) => {
+    // prevent refresh the page till the form is submitted
     event.preventDefault();
+
+    // if authClient is not exist, do nothing
     if (!authClient) return;
 
+    // use the 'await' wait asynchronous operation to complete
     await authClient.login({
       identityProvider: 'https://identity.ic0.app',
       onSuccess: async () => {
+        // get the identity from the authClient
         const identity = authClient.getIdentity();
+        // set the session expiration time to 10 min
         const expiryTime = Date.now() + 1 * 60 * 10000;
+        // get the user's principal from the identity and set it to string value
         setPrincipal(identity.getPrincipal().toText());
         setSessionExpiry(expiryTime);
+        // store the principal and expiration time at the local storage
         localStorage.setItem('principal', identity.getPrincipal().toText());
         localStorage.setItem('expiresAt', expiryTime.toString());
 
+        // add the user's principal to backend canister
         await addUserToBackend(identity.getPrincipal());
       },
       onError: (err) => {
@@ -63,28 +78,40 @@ export const useAuth = () => {
 
   // NFID login
   const loginNFID = async (event) => {
+    // prevent refresh the page till the form is submitted
     event.preventDefault();
+
+    // if authClient is not exist, do nothing
     if (!authClient) return;
 
+    // define a list of canister IDs at the backend for 
     const arrayOfYourBackendCanisters = [
       "cuj6u-c4aaa-aaaaa-qaajq-cai", 
       "cbopz-duaaa-aaaaa-qaaka-cai", 
       "ctiya-peaaa-aaaaa-qaaja-cai"
     ];
 
-    authClient.login({
+    // use the 'await' wait asynchronous operation to complete
+    await authClient.login({
       identityProvider: 'https://nfid.one/authenticate',
-      maxTimeToLive: BigInt(1 * 60 * 10000000000),
+      // set the maximum lifetime of the session to 10 min
+      maxTimeToLive: BigInt(1 * 60 * 100000000000),
+      // set a list of IDs of backend canisters that users can access when they login
       targets: arrayOfYourBackendCanisters,
       
       onSuccess: async () => {
+        // get the identity from the authClient
         const identity = authClient.getIdentity();
+        // set the session expiration time to 10 min
         const expiryTime = Date.now() + 1 * 60 * 10000;
+        // get the user's principal from the identity and set it to string value
         setPrincipal(identity.getPrincipal().toText());
         setSessionExpiry(expiryTime);
+        // store the principal and expiration time at the local storage
         localStorage.setItem('principal', identity.getPrincipal().toText());
         localStorage.setItem('expiresAt', expiryTime.toString());
 
+        // add the user's principal to backend canister
         await addUserToBackend(identity.getPrincipal());
       },
       onError: (err) => {
@@ -96,23 +123,32 @@ export const useAuth = () => {
   // Function to add user to backend
   const addUserToBackend = async (principal) => {
     const agent = new HttpAgent();
+    // get the root key to verify the certification only in the local environment
     await agent.fetchRootKey();
     
+    // create the Actor to interact with backend canister
     const backendActor = Actor.createActor(backend_idlFactory, {
       agent,
       canisterId: backend_canisterId,
     });
 
+    // call the 'add_user' method to store user's principal at backend canister
     await backendActor.add_user(principal);
   };
 
   // logout
   const logout = async () => {
+    // if authClient is not exist, do nothing
     if (!authClient) return;
 
+    // excute logout from authClient
     await authClient.logout();
+
+    // after logout, set principal and expiration time null
     setPrincipal(null);
     setSessionExpiry(null);
+
+    // after logout, remove principal and expiration time from local storage
     localStorage.removeItem('principal');
     localStorage.removeItem('expiresAt');
   };
